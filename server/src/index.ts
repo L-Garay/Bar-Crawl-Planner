e613139d5735e7f72e6dcbd8dca69c0966d736b6
Logan <garay.logan@gmail.com>
2022-11-02 09:26:20 -0600

    can use auth0 to handle logins and logouts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,18 @@
 import { ApolloServer } from '@apollo/server';
-import { startStandaloneServer } from '@apollo/server/standalone';
 import resolvers from './resolvers';
 import { PrismaClient } from '@prisma/client';
 // import { readFileSync } from 'fs';
 import typeDefs from './schema';
+import { auth, requiresAuth } from 'express-openid-connect';
+import path from 'path';
+import { expressMiddleware } from '@apollo/server/express4';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+import express from 'express';
+import http from 'http';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import expressJwt from 'express-jwt';
+require('dotenv').config();
 
 export const prismaClient = new PrismaClient();
 
@@ -13,22 +22,58 @@ export const prismaClient = new PrismaClient();
 //   encoding: 'utf-8',
 // });
 
-// The ApolloServer constructor requires two parameters: your schema
-// definition and your set of resolvers.
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-});
-
-// Passing an ApolloServer instance to the `startStandaloneServer` function:
-//  1. creates an Express app
-//  2. installs your ApolloServer instance as middleware
-//  3. prepares your app to handle incoming requests
 async function StartServer() {
-  const { url } = await startStandaloneServer(server, {
-    listen: { port: 4000 },
+  // Required logic for integrating with Express
+  const app = express();
+  // Our httpServer handles incoming requests to our Express app.
+  // Below, we tell Apollo Server to "drain" this httpServer,
+  // enabling our servers to shut down gracefully.
+  const httpServer = http.createServer(app);
+
+  // Same ApolloServer initialization as before, plus the drain plugin
+  // for our httpServer.
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
   });
-  console.log(`🚀  Server ready at: ${url}`);
+  // Ensure we wait for our server to start
+  await server.start();
+
+  // Set up our Express middleware to handle CORS, body parsing,
+  // and our expressMiddleware function.
+  app.use('/playground', cors(), bodyParser.json(), expressMiddleware(server));
+  console.log(process.env.BASE_URL);
+  app.use(
+    auth({
+      issuerBaseURL: process.env.AUTH0_ISSUER_URL,
+      baseURL: process.env.BASE_URL,
+      clientID: process.env.AUTH_CLIENT_ID,
+      secret: process.env.SESSION_SECRET,
+      authRequired: false,
+      auth0Logout: true,
+    })
+  );
+
+  app.get('/', (req, res) => {
+    const message = req.oidc.isAuthenticated()
+      ? `Hello ${req.oidc.user?.name}, you have sucessfully logged in!`
+      : 'You are logged out.';
+    res.send(message);
+  });
+
+  app.get('/protected', requiresAuth(), (req, res) => {
+    res.send(JSON.stringify(req.oidc.user));
+  });
+
+  app.set('trust proxy', true);
+
+  const port = process.env.SERVER_PORT || 4000;
+  // Modified server startup
+  await new Promise((resolve) =>
+    httpServer.listen({ port: port }, () => resolve)
+  );
+  console.log(`🚀  Server ready at http://localhost:${port}/`);
 }
 
 StartServer();
---
+++ server/src/validate.ts
@@ -0,0 +1,44 @@
+import dotenv from 'dotenv';
+import jwt from 'jsonwebtoken';
+import jwksClient from 'jwks-rsa';
+
+const client = jwksClient({
+  jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`,
+});
+
+const getKey = (header: any, callback: any) => {
+  client.getSigningKey(header.kid, (error, key) => {
+    const signingKey = key?.getPublicKey();
+    callback(null, signingKey);
+  });
+};
+
+const isTokenValid = async (token: string) => {
+  if (token) {
+    const bearerToken = token.split(' ');
+
+    const result = new Promise((resolve, reject) => {
+      jwt.verify(
+        bearerToken[1],
+        getKey,
+        {
+          audience: process.env.API_IDENTIFIER,
+          issuer: `https://${process.env.AUTH0_DOMAIN}/`,
+          algorithms: ['RS256'],
+        },
+        (error, decoded) => {
+          if (error) {
+            resolve({ error });
+          }
+          if (decoded) {
+            resolve({ decoded });
+          }
+        }
+      );
+    });
+    return result;
+  }
+  return { error: 'No token provided' };
+};
+
+export default isTokenValid;
