5fcc36d245204721bb2a937bfd552e03f8f99b6f
Logan <garay.logan@gmail.com>
2022-11-15 13:54:39 -0700

    initial auth0 env variable setup and client creation

---
+++ client/app/auth/authClient.tsx
@@ -0,0 +1,21 @@
+import type { Auth0Client } from '@auth0/auth0-spa-js';
+import { createAuth0Client } from '@auth0/auth0-spa-js';
+import getConfig from '../utils/getConfig';
+
+const config = getConfig();
+
+let _auth0Client: Auth0Client;
+
+const createClient = async () => {
+  if (_auth0Client) return _auth0Client;
+  _auth0Client = await createAuth0Client({
+    domain: config.DOMAIN,
+    clientId: config.CLIENT_ID,
+  });
+  return _auth0Client;
+};
+
+export default async function getAuth0Client() {
+  const client = await createClient();
+  return client;
+}
