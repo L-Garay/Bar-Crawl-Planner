70debc970d37a1baaf72451015975ce2c54dd56f
Logan <garay.logan@gmail.com>
2022-11-30 12:54:45 -0700

    Simplify authenticator code and update user type

--- client/app/auth/authenticator.ts
+++ client/app/auth/authenticator.ts
@@ -2,14 +2,16 @@ import { redirect } from '@remix-run/node';
 import { Authenticator } from 'remix-auth';
 import { Auth0Strategy } from 'remix-auth-auth0';
 import getConfig from '~/utils/config.server';
-import type { User } from '../types/sharedTypes';
+import type { Auth0User } from '../types/sharedTypes';
 import { getSession, destroySession, sessionStorage } from './session';
 
 const config = getConfig();
 
 // Create an instance of the authenticator, pass a generic with what your
 // strategies will return and will be stored in the session
-export const authenticator = new Authenticator<User | null>(sessionStorage);
+export const authenticator = new Authenticator<Auth0User | null>(
+  sessionStorage
+);
 
 let auth0Strategy = new Auth0Strategy(
   {
@@ -21,26 +23,23 @@ let auth0Strategy = new Auth0Strategy(
   },
   // Sets the token header
   async (authData) => {
-    const idToken = authData.extraParams.id_token;
+    // NOTE I don't believe we can/should do this user fetching here
+    // at this point we should only get and set the authData in session
+    // that way we can then fetch that data throughout the rest of the app
+    // then on the homepage we can use the authenticator.isAuthenticated() method to check to see if there is a 'user'/authData set
+    // if NOT then we can redirect them back to the landing page as that would indicate something went wrong
+    // if the data IS SET then we can use that idToken to then create the apollo client
 
-    // Get the user data from your DB or API using the tokens and profile
-    // NOTE the returned data object to have atleast the user's email and name is optional
+    // const response = await fetch(`${config.SERVER.ADDRESS}/authenticate`, {
+    //   headers: {
+    //     Authorization: `Bearer ${idToken}`,
+    //   },
+    // });
+    // if (response.status === 403) return null;
+    // const userData = await response.json();
+    // console.log(userData);
 
-    const response = await fetch(`${config.SERVER.ADDRESS}/authenticate`, {
-      headers: {
-        Authorization: `Bearer ${idToken}`,
-      },
-    });
-
-    if (response.status === 403) return null;
-
-    const userData = await response.json();
-    console.log(userData);
-    return {
-      token: 'foo0',
-      email: 'bar.com',
-    };
-    // return { token: idToken, email: userData.email, name: userData.name };
+    return { ...authData };
   }
 );
 
--- client/app/types/sharedTypes.ts
+++ client/app/types/sharedTypes.ts
@@ -1,5 +1,10 @@
-export type User = {
-  email: string;
-  name?: string;
-  token: string;
+import type { AppLoadContext } from '@remix-run/node';
+import type { Auth0ExtraParams, Auth0Profile } from 'remix-auth-auth0';
+
+export type Auth0User = {
+  accessToken: string;
+  extraParams: Auth0ExtraParams;
+  refreshToken?: string;
+  context?: AppLoadContext;
+  profile: Auth0Profile;
 };
