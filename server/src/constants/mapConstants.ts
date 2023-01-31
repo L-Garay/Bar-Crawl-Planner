4509b6e84cc35e7deb01c7035635e3dbe27e7a42
Logan <garay.logan@gmail.com>
2023-01-31 16:37:32 -0700

    start working on main map search query

---
+++ server/src/constants/mapConstants.ts
@@ -0,0 +1,22 @@
+export const CITY_COORDINATES = {
+  boise: {
+    lat: 43.6141397,
+    lng: -116.2155451,
+  },
+  denver: {
+    lat: 39.7465108,
+    lng: -104.9968927,
+  },
+  slc: {
+    lat: 40.762807,
+    lng: -111.9084849,
+  },
+  portland: {
+    lat: 45.5142438,
+    lng: -122.6879714,
+  },
+  seattle: {
+    lat: 47.6053827,
+    lng: -122.3530897,
+  },
+};
