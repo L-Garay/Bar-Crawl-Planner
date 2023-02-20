6051cc11e1d291cd005661e6a77270ed7bef9dd0
Logan <garay.logan@gmail.com>
2023-02-20 14:41:29 -0700

    refactor outingInvite page and cleanup login page

--- client/app/routes/__layouts/login.tsx
+++ client/app/routes/__layouts/login.tsx
@@ -4,43 +4,9 @@ import type { ActionFunction, LoaderFunction } from '@remix-run/node';
 import { redirect } from '@remix-run/node';
 import { authenticator } from '~/auth/authenticator';
 import { validateUserAndSession } from '~/utils/validateUserAndSession';
-import fs from 'fs';
-import { useEffect, useState } from 'react';
 
 export const action: ActionFunction = async ({ params, request }) => {
-  const url = new URL(request.url);
-  const profileId = url.searchParams.get('profileId');
-  const outingId = url.searchParams.get('outingId');
-  const returnTo = url.searchParams.get('returnTo');
-  const data = {
-    returnTo,
-    profileId,
-    outingId,
-  };
-  // NOTE this is temporary until we can log in - get redirected to outing page - then delete file
-  // TODO change this
-  try {
-    fs.unlinkSync('/tmp/barcrawl');
-  } catch (error) {
-    console.log('Error deleting file', error);
-  }
-  if (profileId != null && returnTo != null) {
-    try {
-      fs.writeFileSync('/tmp/barcrawl', JSON.stringify(data));
-    } catch (error) {
-      console.log('Error writing to file', error);
-    }
-  }
-
-  // we need to check the visitor's against those values in the database
-  // if they match, we know this visitor is the one who was invited
-  // which means we can connect them to the account, profile (the profile will have already been added to the outing)
-  // they'll still need to go through the auth flow and register their identity with Auth0 however
-  // NOTE this raises the potential issue of someone registering an identity with a different email address than the one they were invited with and that is currently stored on their pre-made account in the DB
-  // TODO take the invite account data that was submitted by the form and add it to the authenticator context
-  return authenticator.authenticate('auth0', request, {
-    context: {},
-  });
+  return authenticator.authenticate('auth0', request);
 };
 
 export const loader: LoaderFunction = async ({ params, request }) => {
@@ -64,14 +30,6 @@ export const loader: LoaderFunction = async ({ params, request }) => {
 };
 
 export default function LoginPage() {
-  const [accountData, setAccountData] = useState<Record<string, string>>({});
-
-  useEffect(() => {
-    // TODO get the invite account data from local storage and set it to state
-    // then we'll need to attach it to the action function call from the button
-    // most likely we'll need to tie the accountData to a hidden input that is submitted by the Form
-  }, [window]);
-
   return (
     <>
       <div
---
+++ client/app/routes/__layouts/outinginvite.tsx
@@ -0,0 +1,107 @@
+import { gql, useMutation } from '@apollo/client';
+import type { LoaderFunction } from '@remix-run/node';
+import { useLoaderData, useNavigate } from '@remix-run/react';
+import { useEffect } from 'react';
+import getConfig from '~/utils/config.server';
+import { validateUserAndSession } from '~/utils/validateUserAndSession';
+
+export const CONNECT_PROFILE = gql`
+  mutation DisconnectUserWithOuting($outing_id: Int!, $profile_id: Int!) {
+    DisconnectUserWithOuting(outing_id: $outing_id, profile_id: $profile_id) {
+      id
+      name
+    }
+  }
+`;
+
+export const loader: LoaderFunction = async ({ params, request, context }) => {
+  const url = new URL(request.url);
+  const outingId = url.searchParams.get('outingId');
+  const profileId = url.searchParams.get('profileId');
+  const socialPin = url.searchParams.get('socialPin');
+
+  const config = getConfig();
+  const { valid } = await validateUserAndSession(request);
+
+  return { outingId, profileId, socialPin, isValid: valid, config };
+};
+
+export default function OutingInvite() {
+  const { outingId, profileId, socialPin, isValid, config } = useLoaderData();
+  const navigate = useNavigate();
+
+  const [
+    DisconnectProfile,
+    {
+      loading: connectionLoading,
+      error: connectionError,
+      data: connectionData,
+    },
+  ] = useMutation(CONNECT_PROFILE);
+
+  useEffect(() => {
+    if ((outingId || profileId || socialPin) && !isValid && window) {
+      const inviteData = {
+        outingId,
+        profileId,
+        socialPin,
+        returnTo: `/outings/my-outings/${outingId}`,
+      };
+      console.log('setting invite data in local storage', inviteData);
+
+      window.localStorage.setItem('inviteData', JSON.stringify(inviteData));
+    }
+  }, [outingId, profileId, socialPin, isValid]);
+
+  const handleAccept = async () => {
+    if (isValid) {
+      navigate(`/outings/my-outings/${outingId}`);
+    } else {
+      const inviteData = window.localStorage.getItem('inviteData');
+      if (!inviteData) {
+        const inviteData = {
+          outingId,
+          profileId,
+          socialPin,
+          returnTo: `/outings/my-outings/${outingId}`,
+        };
+        window.localStorage.setItem('inviteData', JSON.stringify(inviteData));
+      }
+      navigate(`/login`);
+    }
+  };
+
+  const handleDecline = async () => {
+    if (isValid) {
+      await DisconnectProfile({
+        variables: {
+          profile_id: Number(profileId),
+          outing_id: Number(outingId),
+        },
+      });
+      window.localStorage.removeItem('inviteData');
+      navigate('/outings/my-outings');
+    } else {
+      await fetch(`${config.SERVER.ADDRESS}/disconnect-user`, {
+        method: 'POST',
+        body: `profileId=${profileId}&outingId=${outingId}`,
+      });
+      window.localStorage.removeItem('inviteData');
+      navigate('/');
+    }
+  };
+
+  return (
+    <div>
+      <h1>Outing Invite Page</h1>
+      <div>
+        {/* TODO put outing details here */}
+        <p>outing details will go here</p>
+      </div>
+      <div>
+        <button onClick={handleAccept}>Accept</button>
+        <button onClick={handleDecline}>Decline</button>
+      </div>
+    </div>
+  );
+}
