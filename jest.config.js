32178ac0367a8456ca14fbd5a198d6f457f0bbbb
Logan <garay.logan@gmail.com>
2023-03-27 14:25:47 -0600

    add missing /unit directory in ignore path

--- jest.config.js
+++ jest.config.js
@@ -159,7 +159,7 @@ module.exports = {
   // ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-  testPathIgnorePatterns: ['/node_modules/', '/tests/mutations'],
+  testPathIgnorePatterns: ['/node_modules/', '/tests/unit/mutations'],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
   // testRegex: [],
