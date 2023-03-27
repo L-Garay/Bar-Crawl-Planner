2d73ce22c04e1ff985f096d20826c36715ded023
Logan <garay.logan@gmail.com>
2023-03-27 14:23:02 -0600

    attempt to ignore mutation tests in jest config

--- server/src/tests/unit/mutations/accountMutationTests.test.ts
+++ server/src/tests/unit/mutations/accountMutationTests.test.ts
@@ -5,32 +5,32 @@ import { prismaMock } from '../../../utilities/prismaClientSingleton';
 // which then fails because that file has a bunch of dependencies that are not mocked
 // however, this should just be testing the prisma client and methods, shouldn't need the resolver or graphql at all
 // NOTE not sure if this test actually passes or not yet
-// describe.skip('accountMutations', () => {
-//   describe.skip('CreateAccount', () => {
-//     it.skip('should create an account', async () => {
-//       const account = {
-//         id: 1,
-//         email: 'test@mail.com',
-//         email_verified: true,
-//         created_at: new Date().toISOString(),
-//         deactivated: false,
-//         deactivated_at: '',
-//         phone_number: '',
-//       };
-//       // @ts-ignore
-//       prismaMock.account.create.mockResolvedValue(account);
+describe('accountMutations', () => {
+  describe('CreateAccount', () => {
+    it('should create an account', async () => {
+      const account = {
+        id: 1,
+        email: 'test@mail.com',
+        email_verified: true,
+        created_at: new Date().toISOString(),
+        deactivated: false,
+        deactivated_at: '',
+        phone_number: '',
+      };
+      // @ts-ignore
+      prismaMock.account.create.mockResolvedValue(account);
 
-//       await expect(
-//         CreateAccount(account.email, account.email_verified)
-//       ).resolves.toEqual({
-//         id: 1,
-//         email: 'test@mail.com',
-//         email_verified: true,
-//         created_at: new Date().toISOString(),
-//         deactivated: false,
-//         deactivated_at: '',
-//         phone_number: '',
-//       });
-//     });
-//   });
-// });
+      await expect(
+        CreateAccount(account.email, account.email_verified)
+      ).resolves.toEqual({
+        id: 1,
+        email: 'test@mail.com',
+        email_verified: true,
+        created_at: new Date().toISOString(),
+        deactivated: false,
+        deactivated_at: '',
+        phone_number: '',
+      });
+    });
+  });
+});
